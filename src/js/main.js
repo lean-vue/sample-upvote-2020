cd609e2facbb3c1b1c54fb36664c3d8a63a1a218
Michael Alt <mail@lean-stack.de>
2021-01-10 19:12:48 +0100

    Divide the app into components. First: global components.

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,62 @@
-console.log('Your starting point. Enjoy the ride!');
-new Vue({
-  el: '#app',
-  data: {
-    submissions: window.Seed.submissions
-  },
+Vue.component('submission-list', {
+  template: `
+  <div class="section">
+    <article v-for="s in sortedSubmissions" :key="s.id" class="media">
+      <figure class="media-left">
+        <img class="image is-64x64" :src="s.submissionImage">
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>
+              <a :href="s.url" class="has-text-info">{{ s.title }}</a>
+              <span class="tag is-small">#{{ s.id }}</span>
+            </strong>
+            <br>
+            {{ s.description }}
+            <br>
+            <small class="is-size-7">
+              Submitted by:
+              <img class="image is-24x24" :src="s.avatar">
+            </small>
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span class="icon is-small" @click="voteFor(s)">
+          <i class="fa fa-chevron-up"></i>
+          <strong class="has-text-info">{{ s.votes }}</strong>
+        </span>
+      </div>
+    </article>
+  </div>
+  `,
+  props: ['submissions'],
   methods: {
-    voteFor: function(submission) {
+    voteFor: function (submission) {
       ++submission.votes;
     }
   },
   computed: {
-    sortedSubmissions: function() {
-      return [...this.submissions].sort((s1,s2) => s2.votes - s1.votes);
+    sortedSubmissions: function () {
+      return [...this.submissions].sort((s1, s2) => s2.votes - s1.votes);
     }
   }
+});
+
+const upvoteApp = Vue.component('upvote-app', {
+  template: `
+  <div>
+    <h2 class="title has-text-centered dividing-header">UpVote!</h2>
+    <submission-list :submissions="submissions"></submission-list>
+  </div>
+  `,
+  data: () => ({
+    submissions: window.Seed.submissions
+  })
+});
+
+new Vue({
+  el: '#app',
+  template: '<upvote-app></upvote-app>'
 });
