af32a12a8982e27a832c7d558133adaef994e8a8
Michael Alt <mail@lean-stack.de>
2021-01-10 19:23:44 +0100

    Add two local components

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,49 @@
+const SubmissionVotes = {
+  template: `
+    <span class="icon is-small" @click="$emit('vote')">
+          <i class="fa fa-chevron-up"></i>
+          <strong class="has-text-info">{{ submission.votes }}</strong>
+    </span>
+  `,
+  props: ['submission']
+}
+
+const SubmissionContent = {
+  template: `
+    <div class="content">
+      <p>
+        <strong>
+          <a :href="submission.url" class="has-text-info">{{ submission.title }}</a>
+          <span class="tag is-small">#{{ submission.id }}</span>
+        </strong>
+        <br>
+        {{ submission.description }}
+        <br>
+        <small class="is-size-7">
+          Submitted by:
+          <img class="image is-24x24" :src="submission.avatar">
+        </small>
+      </p>
+    </div>
+  `,
+  props: ['submission']
+}
+
 Vue.component('submission-list', {
   template: `
-  <div class="section">
-    <article v-for="s in sortedSubmissions" :key="s.id" class="media">
-      <figure class="media-left">
-        <img class="image is-64x64" :src="s.submissionImage">
-      </figure>
-      <div class="media-content">
-        <div class="content">
-          <p>
-            <strong>
-              <a :href="s.url" class="has-text-info">{{ s.title }}</a>
-              <span class="tag is-small">#{{ s.id }}</span>
-            </strong>
-            <br>
-            {{ s.description }}
-            <br>
-            <small class="is-size-7">
-              Submitted by:
-              <img class="image is-24x24" :src="s.avatar">
-            </small>
-          </p>
+    <div class="section">
+      <article v-for="s in sortedSubmissions" :key="s.id" class="media">
+        <figure class="media-left">
+          <img class="image is-64x64" :src="s.submissionImage">
+        </figure>
+        <div class="media-content">
+          <submission-content :submission="s"></submission-content>
         </div>
-      </div>
-      <div class="media-right">
-        <span class="icon is-small" @click="voteFor(s)">
-          <i class="fa fa-chevron-up"></i>
-          <strong class="has-text-info">{{ s.votes }}</strong>
-        </span>
-      </div>
-    </article>
-  </div>
+        <div class="media-right">
+          <submission-votes :submission="s" @vote="voteFor(s)"></submission-votes>
+        </div>
+      </article>
+    </div>
   `,
   props: ['submissions'],
   methods: {
@@ -41,15 +55,19 @@ Vue.component('submission-list', {
     sortedSubmissions: function () {
       return [...this.submissions].sort((s1, s2) => s2.votes - s1.votes);
     }
+  },
+  components: {
+    'submission-votes': SubmissionVotes,
+    SubmissionContent
   }
 });
 
 const upvoteApp = Vue.component('upvote-app', {
   template: `
-  <div>
-    <h2 class="title has-text-centered dividing-header">UpVote!</h2>
-    <submission-list :submissions="submissions"></submission-list>
-  </div>
+    <div>
+      <h2 class="title has-text-centered dividing-header">UpVote!</h2>
+      <submission-list :submissions="submissions"></submission-list>
+    </div>
   `,
   data: () => ({
     submissions: window.Seed.submissions
